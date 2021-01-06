cb687a3c79e43ea2290866ddc1a830718d21ecf7
AlexandraLeonova <alexleonova2013@gmail.com>
2021-01-07 00:14:27 +0300

    black2

--- homework05/vkapi/wall.py
+++ homework05/vkapi/wall.py
@@ -4,7 +4,7 @@ import typing as tp
 from string import Template
 
 import pandas as pd  # type: ignore
-from pandas import json_normalize 
+from pandas import json_normalize
 
 from requests.api import post
 from vkapi import config, session
