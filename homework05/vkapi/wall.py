59ae1c0cd76dbe2419deade3839fe2d6f706cb83
AlexandraLeonova <alexleonova2013@gmail.com>
2021-01-11 20:19:12 +0300

    this?

--- homework05/vkapi/wall.py
+++ homework05/vkapi/wall.py
@@ -5,7 +5,6 @@ from string import Template
 
 import pandas as pd  # type: ignore
 from pandas import json_normalize
-
 from requests.api import post  # type: ignore
 from vkapi import config, session
 from vkapi.exceptions import APIError
